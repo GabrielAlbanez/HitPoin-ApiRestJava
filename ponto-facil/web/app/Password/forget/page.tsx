cc82567b5c2311e011b74bb9ead3b81d7c5117d2
GabrielAlbanez <gabriel.g.albanez@gmail.com>
2024-11-27 08:30:53 -0300

    correct deploy

--- ponto-facil/web/app/Password/forget/page.tsx
+++ ponto-facil/web/app/Password/forget/page.tsx
@@ -7,102 +7,111 @@ import { useState } from "react";
 import { Input, Button } from "@nextui-org/react";
 
 const schemaPasswordReset = z.object({
-    email: z.string().email("Formato de email inválido"),
+  email: z.string().email("Formato de email inválido"),
 });
 
 type FormData = z.infer<typeof schemaPasswordReset>;
 
 const ForgetPassword = () => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [serverError, setServerError] = useState<string | null>(null);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormData>({
-        resolver: zodResolver(schemaPasswordReset),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schemaPasswordReset),
+  });
 
-    const onSubmit: SubmitHandler<FormData> = async (data) => {
-        setLoading(true);
-        setServerError(null);
-        setSuccessMessage(null);
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setLoading(true);
+    setServerError(null);
+    setSuccessMessage(null);
 
-        try {
-            const response = await fetch("https://hitpoint-backend-latest.onrender.com/usuarios/forgot-password", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email: data.email }),
-            });
-
-            // Captura a resposta do corpo como texto
-            const responseText = await response.text();
-            console.log("Resposta da API forgot-password:", responseText);
-
-           if (response.ok && responseText) {
-                if (responseText == Link de redefinição de senha enviado para o e-mail: ${data.email}) {
-                    setSuccessMessage(responseText);
-                }  else{
-                    setServerError(responseText);
-                }
-            } else if (response.status === 401) {
-                setServerError(responseText || "Acesso Negado");
-            } else {
-                setServerError(responseText || "Ocorreu um erro ao tentar enviar o link. Tente novamente.");
-            }
-        } catch (error) {
-            setServerError("Erro de conexão. Por favor, tente novamente.");
+    try {
+      const response = await fetch(
+        "https://hitpoint-backend-latest.onrender.com/usuarios/forgot-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: data.email }),
         }
+      );
 
-        setLoading(false);
-    };
+      // Captura a resposta do corpo como texto
+      const responseText = await response.text();
+      console.log("Resposta da API forgot-password:", responseText);
 
-    return (
-        <form
-            className="w-full h-full flex flex-col gap-5 justify-center items-center"
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <h1 className="text-3xl lg:text-4xl">Redefinir Senha</h1>
-            <p>Digite seu e-mail para receber o link de redefinição de senha.</p>
+      if (response.ok && responseText) {
+        if (
+          responseText ===
+          `Link de redefinição de senha enviado para o e-mail: ${data.email}`
+        ) {
+          setSuccessMessage(responseText);
+        } else {
+          setServerError(responseText);
+        }
+      } else if (response.status === 401) {
+        setServerError(responseText || "Acesso Negado");
+      } else {
+        setServerError(
+          responseText ||
+            "Ocorreu um erro ao tentar enviar o link. Tente novamente."
+        );
+      }
+    } catch (error) {
+      setServerError("Erro de conexão. Por favor, tente novamente.");
+    }
 
-            <div className="w-full">
-                <Input
-                    label="Email"
-                    placeholder="Digite seu email"
-                    variant="underlined"
-                    {...register("email")}
-                    isInvalid={!!errors.email}
-                    size="lg"
-                />
-                {errors.email && (
-                    <span className="text-red-500 text-sm mt-1 ml-2">
-                        {errors.email.message}
-                    </span>
-                )}
-            </div>
+    setLoading(false);
+  };
 
-            {serverError && (
-                <span className="text-red-500 text-sm mt-1">{serverError}</span>
-            )}
-            {successMessage && (
-                <span className="text-green-500 text-sm mt-1">{successMessage}</span>
-            )}
+  return (
+    <form
+      className="w-full h-full flex flex-col gap-5 justify-center items-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h1 className="text-3xl lg:text-4xl">Redefinir Senha</h1>
+      <p>Digite seu e-mail para receber o link de redefinição de senha.</p>
 
-            <Button
-                className="w-full flex items-center justify-center mt-4"
-                color="primary"
-                disabled={loading}
-                size="md"
-                type="submit"
-            >
-                {loading ? "Enviando..." : "Enviar Link"}
-            </Button>
-        </form>
-    );
+      <div className="w-full">
+        <Input
+          label="Email"
+          placeholder="Digite seu email"
+          variant="underlined"
+          {...register("email")}
+          isInvalid={!!errors.email}
+          size="lg"
+        />
+        {errors.email && (
+          <span className="text-red-500 text-sm mt-1 ml-2">
+            {errors.email.message}
+          </span>
+        )}
+      </div>
+
+      {serverError && (
+        <span className="text-red-500 text-sm mt-1">{serverError}</span>
+      )}
+      {successMessage && (
+        <span className="text-green-500 text-sm mt-1">{successMessage}</span>
+      )}
+
+      <Button
+        className="w-full flex items-center justify-center mt-4"
+        color="primary"
+        disabled={loading}
+        size="md"
+        type="submit"
+      >
+        {loading ? "Enviando..." : "Enviar Link"}
+      </Button>
+    </form>
+  );
 };
 
 export default ForgetPassword;
